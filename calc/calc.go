292000dc4eabca55c0ff01d33ce0483fe6101f66
EAD\xtran3 <xtran3@dxc.com>
2023-01-06 14:31:27 +0700

    test module

---
+++ calc/calc.go
@@ -0,0 +1,7 @@
+// You can edit this code!
+// Click here and start typing.
+package calc
+
+func Add(a int, b int) int {
+	return a + b
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/TranLeXuan-DXC/go_math
+
+go 1.19
